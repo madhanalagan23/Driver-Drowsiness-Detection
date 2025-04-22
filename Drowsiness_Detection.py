a3fc3d98bff8bcdff01807dd5527c362d9415068
madhanalagan23 <madhaalagan23@gmail.com>
2025-04-22 09:41:30 +0530

    new

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -40,28 +40,28 @@ while True:
                 shape = predict(gray, subject)  
                 shape = face_utils.shape_to_np(shape)
                 leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
-		ear = (leftEAR + rightEAR) / 2.0
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-		if ear < thresh:
-			flag += 1
-			print (flag)
-			if flag >= frame_check:
-				cv2.putText(frame, "****************ALERT!****************", (10, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				cv2.putText(frame, "****************ALERT!****************", (10,325),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				mixer.music.play()
-		else:
-			flag = 0
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
-cv2.destroyAllWindows()
-cap.release()
+		# rightEye = shape[rStart:rEnd]
+		# leftEAR = eye_aspect_ratio(leftEye)
+		# rightEAR = eye_aspect_ratio(rightEye)
+		# ear = (leftEAR + rightEAR) / 2.0
+		# leftEyeHull = cv2.convexHull(leftEye)
+		# rightEyeHull = cv2.convexHull(rightEye)
+		# cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		# cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+		# if ear < thresh:
+		# 	flag += 1
+# 			print (flag)
+# 			if flag >= frame_check:
+# 				cv2.putText(frame, "****************ALERT!****************", (10, 30),
+# 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+# 				cv2.putText(frame, "****************ALERT!****************", (10,325),
+# 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+# 				mixer.music.play()
+# 		else:
+# 			flag = 0
+# 	cv2.imshow("Frame", frame)
+# 	key = cv2.waitKey(1) & 0xFF
+# 	if key == ord("q"):
+# 		break
+# cv2.destroyAllWindows()
+# cap.release()
